e741bf913573cb0f8213c3d06347ec7e800c5d51
eBenya <koly_07@mail.ru>
2022-03-27 15:41:04 +0300

    use constant

--- ConfigurationGetter/ConfigurationGetter/ConfigurationGetter.cs
+++ ConfigurationGetter/ConfigurationGetter/ConfigurationGetter.cs
@@ -4,16 +4,16 @@ namespace ConfigurationGetter
 {
     public static class ConfigurationGetter
     {
-        private const int rootDepht = 0;
-        private const int firstDepht = 0;
+        private const int ROOT_DEPHT = 0;
+        private const int FIRST_DEPHT = 0;
 
         private static int _depht;
         public static string GenerateConfigurationString(this IConfiguration configuration, int depht = int.MaxValue)
         {
             _depht = depht;
-            return $"{{\n{GetSectionContent(configuration, 1)}}}";
+            return $"{{\n{GetSectionContent(configuration)}}}";
         }
-        private static string GetSectionContent(this IConfiguration configSection, int currentDepht = 1)
+        private static string GetSectionContent(this IConfiguration configSection, int currentDepht = FIRST_DEPHT)
         {
             if (currentDepht == _depht)
                 return string.Empty;
