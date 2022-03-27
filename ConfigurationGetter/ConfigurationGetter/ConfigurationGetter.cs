59c7c3521f6d5bd7fbef058dc6fe372c21e11c11
eBenya <koly_07@mail.ru>
2022-03-27 15:39:20 +0300

    static configuration info getter

---
+++ ConfigurationGetter/ConfigurationGetter/ConfigurationGetter.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ConfigurationGetter
+{
+    public static class ConfigurationGetter
+    {
+        private const int rootDepht = 0;
+        private const int firstDepht = 0;
+
+        private static int _depht;
+        public static string GenerateConfigurationString(this IConfiguration configuration, int depht = int.MaxValue)
+        {
+            _depht = depht;
+            return $"{{\n{GetSectionContent(configuration, 1)}}}";
+        }
+        private static string GetSectionContent(this IConfiguration configSection, int currentDepht = 1)
+        {
+            if (currentDepht == _depht)
+                return string.Empty;
+
+            var indent = new string('\t', currentDepht);
+
+            string sectionContent = "";
+            foreach (var section in configSection.GetChildren())
+            {
+                sectionContent += $"{indent}\"{section.Key}\":";
+                if (section.Value == null)
+                {
+                    string subSectionContent = GetSectionContent(section, currentDepht + 1);
+                    sectionContent += $"{{\n{subSectionContent}{indent}}},\n";
+                }
+                else
+                {
+                    sectionContent += $"\"{section.Value}\",\n";
+                }
+            }
+            return sectionContent;
+        }
+    }
+}
---
+++ ConfigurationGetter/ConsoleWriter/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConfigurationGetter;
+using Microsoft.Extensions.Configuration;
+
+var conf = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddJsonFile("appsettings.Development.json")
+    .Build();
+
+var confString = conf.GenerateConfigurationString(5);
+
+Console.WriteLine(confString);
